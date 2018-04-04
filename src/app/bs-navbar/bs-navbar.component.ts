1c6b7b4a0a69fe8e8b4146fd85030bb8965aebb4
Rares Nesa <raresn@softinfo.ro>
2018-04-04 15:24:33 +0300

    L290 - Added signout and refactored login

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
+  user$: Observable<firebase.User>;
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {
+    this.user$ = afAuth.authState;
+  }
 
-  ngOnInit() {
+  logout() {
+    this.afAuth.auth.signOut();
   }
 
 }
