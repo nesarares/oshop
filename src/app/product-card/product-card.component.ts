ed0d65e370c5a327d9ff74d8c304ae0130481fcd
Rares Nesa <raresn@softinfo.ro>
2018-04-24 17:24:25 +0300

    335 - Added shopping cart adding from main page

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from '../models/product';
+import { ShoppingCartService } from '../shopping-cart.service';
 
 @Component({
   selector: 'product-card',
@@ -9,6 +10,21 @@ import { Product } from '../models/product';
 export class ProductCardComponent {
   @Input('product') product: Product;
   @Input('show-actions') showActions: boolean = true;
+  @Input('shopping-cart') shoppingCart;
 
-  constructor() { }
+  constructor(private cartService: ShoppingCartService) { }
+
+  addToCart() {
+    this.cartService.addToCart(this.product);
+  }
+
+  removeFromCart() {
+    this.cartService.removeFromCart(this.product);
+  }
+
+  getQuantity() {
+    if (!this.shoppingCart) return 0;
+    let item = this.shoppingCart.items[this.product.$key];
+    return item ? item.quantity : 0;
+  }
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,22 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Product } from '../models/product';
 import { ActivatedRoute } from '@angular/router';
 import 'rxjs/add/operator/switchMap';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit, OnDestroy {
   products: Product[] = [];
   filteredProducts: Product[] = [];
   category: string;
+  cart: any;
+  subscription: Subscription;
 
   constructor(
     route: ActivatedRoute,
-    productService: ProductService) {
+    productService: ProductService,
+    private cartService: ShoppingCartService) {
+
     productService
       .getAll()
       .switchMap(products => {
@@ -27,10 +33,17 @@ export class ProductsComponent {
         this.category = params.get('category');
 
         this.filteredProducts = (this.category) ?
-          this.products.filter(p => p.category === this.category) : 
+          this.products.filter(p => p.category === this.category) :
           this.products;
       });
-      
   }
 
+  async ngOnInit() {
+    this.subscription = (await this.cartService.getCart())
+      .subscribe(cart => this.cart = cart);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
